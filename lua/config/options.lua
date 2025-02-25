cf095624b30643049867b1718a035f40987ddbcd
Leonie Scheidweiler <leonie.olivia.scheidweiler@gmail.com>
2025-02-25 21:32:58 +0100

    4 spaces

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.opt.number = true -- Enable line numbers
+vim.opt.relativenumber = false -- Relative numbers
+vim.opt.tabstop = 4 -- Use 4 spaces per tab
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.g.snacks_animate = false
